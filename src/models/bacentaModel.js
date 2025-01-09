42d05a1b4d9489747bb5e60c3b1d21b605074f18
Vida Ackom <61285959+VACKOM@users.noreply.github.com>
2025-01-09 09:53:56 +0000

    Added Center field in bacenta registration form

--- src/models/bacentaModel.js
+++ src/models/bacentaModel.js
@@ -4,6 +4,7 @@ const bacentaSchema = new mongoose.Schema({
   bacentaName: { type: String, required: true },
   bacentaLeader: { type: String, required: true },
   zone: { type: String, required: true },
+  center: { type: String, required: true },
   bacentaID: { type: String, required: true },
   bacentaLocation: { type: String, required: true },
   bacentaContact: { type: String, required: true },
